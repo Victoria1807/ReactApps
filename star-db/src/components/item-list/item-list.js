d81a28b5cc1318f0adff6e1701c9502f52503f84
Victoria1807 <flamaster1807@gmail.com>
2019-01-20 13:54:15 +0200

    Refactoring, cloning elements

--- star-db/src/components/item-list/item-list.js
+++ star-db/src/components/item-list/item-list.js
@@ -25,13 +25,13 @@ export default class ItemList extends Component {
   renderItems(arr) {
     return arr.map((item) => {
       const { id } = item;
+      const label = this.props.children(item);
 
-      const label = this.props.renderItem(item);
       return (
         <li className="list-group-item"
             key={id}
             onClick={() => this.props.onItemSelected(id)}>
-          { label }
+          {label}
         </li>
       );
     });
