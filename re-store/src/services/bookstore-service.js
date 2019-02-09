f8497749a51b7f47c67a4d067f38b7d01f652696
Ostap <flamaster1807@gmail.com>
2019-02-09 21:17:17 +0200

    Created base project frame

--- re-store/src/services/bookstore-service.js
+++ re-store/src/services/bookstore-service.js
@@ -2,6 +2,17 @@
 export default class BookstoreService {
 
   getBooks() {
-    return [];
+    return [
+      {
+        id: 1,
+        title: 'Test1',
+        author: 'AuthorTest1'
+      },
+      {
+        id: 2,
+        title: 'Test2',
+        author: 'AuthorTest2'
+      }
+    ];
   }
 }
